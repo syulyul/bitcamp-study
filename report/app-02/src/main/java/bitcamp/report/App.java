e2e0a32e47275a74b4bc93bc84d53dbe8d698a71
syulyul <syul9800@gmail.com>
2023-06-05 20:47:16 +0900

    .

--- report/app-02/src/main/java/bitcamp/report/App.java
+++ report/app-02/src/main/java/bitcamp/report/App.java
@@ -2,11 +2,13 @@ package bitcamp.report;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("편의점 재고 관리 시스템");
+        System.out.println("마트 재고 관리 시스템");
         System.out.println("----------------------------");
 
-        System.out.print("물품: ");
+        System.out.print("물품 번호: ");
         System.out.println(100);
+
+        
         
     }
 }
