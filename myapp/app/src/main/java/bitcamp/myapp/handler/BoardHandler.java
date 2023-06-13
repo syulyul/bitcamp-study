32004bae716228cfed93b69c02e9dd18f1bade34
syulyul <syul9800@gmail.com>
2023-06-13 21:11:52 +0900

    과

--- myapp/app/src/main/java/bitcamp/myapp/handler/BoardHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/BoardHandler.java
@@ -7,7 +7,7 @@ public class BoardHandler {
 
   static final int MAX_SIZE = 100; // 대문자로 해주는 이유 : 강조하기 위함
   static Board[] boards = new Board[MAX_SIZE];
-  static int length = 0; // 한 사람 입력 할 떄마다 증가시킬 값
+  static int length = 0; // 한 사람 입력 할 때마다 증가시킬 값
 
   public static void inputBoard() {
     if (!available()) {
@@ -40,41 +40,49 @@ public class BoardHandler {
   }
 
   public static void viewBoard() {
-    // String memberNo = Prompt.inputString("번호? ");
-    // // 입력 받은 번호를 가지고 배열에서 해당 회원을 찾아야 한다.
-    // for (int i = 0; i < length; i++) {
-    // Board m = boards[i];
-    // if (m.getNo() == Integer.parseInt(memberNo)) {
-    // // i 번째 항목에 저장된 회원 정보 출력
-    // System.out.printf("이름: %s\n", m.getName());
-    // System.out.printf("이메일: %s\n", m.getEmail());
-    // System.out.printf("성별: %s\n", toGenderString(m.getGender()));
-    // return;
-    // }
-    // }
-    // System.out.println("해당 번호의 회원이 없습니다!");
+    String boardNo = Prompt.inputString("번호? ");
+    // 입력 받은 번호를 가지고 배열에서 해당 게시글을 찾아야 한다.
+    for (int i = 0; i < length; i++) {
+      Board board = boards[i];
+      if (board.getNo() == Integer.parseInt(boardNo)) {
+        // i 번째 항목에 저장된 게시글 정보 출력
+        System.out.printf("제목: %s\n", board.getTitle());
+        System.out.printf("내용: %s\n", board.getContent());
+        System.out.printf("작성자: %s\n", board.getWriter());
+        System.out.printf("조회수: %d\n", board.getViewCount());
+        System.out.printf("등록일: %tY-%1$tm-%1$td\n", board.getCreatedDate());
+        return;
+      }
+    }
+    System.out.println("해당 번호의 게시글이 없습니다!");
   }
 
-  public static String toGenderString(char gender) {
-    return gender == 'M' ? "남성" : "여성";
-  }
+  // public static String toGenderString(char gender) {
+  // return gender == 'M' ? "남성" : "여성";
+  // }
 
   public static void updateBoard() {
-    // String memberNo = Prompt.inputString("번호? ");
-    // // 입력 받은 번호를 가지고 배열에서 해당 회원을 찾아야 한다.
-    // for (int i = 0; i < length; i++) {
-    // Board m = boards[i];
-    //
-    // if (m.getNo() == Integer.parseInt(memberNo)) {
-    // // i 번째 항목에 저장된 회원 정보 출력
-    // m.setName(Prompt.inputString("이름(" + m.getName() + ")? "));
-    // m.setEmail(Prompt.inputString("이메일(" + m.getEmail() + ")?\n"));
-    // m.setPassword(Prompt.inputString("새암호? "));
-    // m.setGender(inputGender(m.getGender()));
-    // return;
-    // }
-    // }
-    // System.out.println("해당 번호의 회원이 없습니다!");
+    String boardNo = Prompt.inputString("번호? ");
+    // 입력 받은 번호를 가지고 배열에서 해당 게시글을 찾아야 한다.
+
+    for (int i = 0; i < length; i++) {
+      Board board = boards[i];
+      if (Prompt.inputString("암호? ").equals(board.getPassword())) {
+        if (board.getNo() == Integer.parseInt(boardNo)) {
+          // i 번째 항목에 저장된 회원 정보 출력
+          System.out.printf("제목(%s)? ", board.getTitle());
+          board.setTitle(Prompt.inputString(""));
+          System.out.printf("내용(%s)? ", board.getContent());
+          board.setContent(Prompt.inputString(""));
+          board.setPassword(Prompt.inputString("새암호? "));
+          return;
+        }
+      } else {
+        System.out.println("암호가 올바르지 않습니다.");
+        return;
+      }
+    }
+    System.out.println("해당 번호의 회원이 없습니다!");
   }
 
   // private static char inputGender(char gender) {
@@ -100,50 +108,31 @@ public class BoardHandler {
   // }
 
   public static void deleteBoard() {
-    // 삭제하려는 회원의 정보가 들어 있는 인덱스를 알아낸다.
+    // 삭제하려는 게시글의 정보가 들어 있는 인덱스를 알아낸다.
 
-    int memberNo = Integer.parseInt(Prompt.inputString("번호? "));
+    int boardNo = Integer.parseInt(Prompt.inputString("번호? "));
 
-    int deletedIndex = indexOf(memberNo);
-
-    // // 입력 받은 번호를 가지고 배열에서 해당 회원을 찾아야 한다.
-    // for (int i = 0; i < length; i++) {
-    // if (no[i] == Integer.parseInt(memberNo)) {
-    // deletedIndex = i;
-    // break;
-    // }
-    // }
+    int deletedIndex = indexOf(boardNo);
 
     if (deletedIndex == -1) {
       System.out.println("해당 번호의 회원이 없습니다!");
       return;
     }
 
-    // if (deletedIndex < (length - 1)) {
-    // // 만약 삭제하려는 항목이 마지막 인덱스의 항목이라면 마지막 인덱스의 값만 0으로 초기화시킨다.
-    // no[deletedIndex] = 0;
-    // name[deletedIndex] = null;
-    // email[deletedIndex] = null;
-    // password[deletedIndex] = null;
-    // gender[deletedIndex] = (char) 0;
-    // } else {
-    // 그 밖에는 해당 인덱스부터 반복하면서 앞 인덱스의 값을 당겨온다.
     for (int i = deletedIndex; i < length - 1; i++) {
       boards[i] = boards[i + 1];
     }
 
     boards[--length] = null;
-    // }
-    // length를 하나 줄인다.
 
     System.out.println("삭제했습니다.");
     return;
   }
 
-  private static int indexOf(int memberNo) {
+  private static int indexOf(int boardNo) {
     for (int i = 0; i < length; i++) {
-      Board m = boards[i];
-      if (m.getNo() == memberNo) {
+      Board board = boards[i];
+      if (board.getNo() == boardNo) {
         return i;
       }
     }
