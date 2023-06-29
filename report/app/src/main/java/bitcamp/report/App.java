37d29cc766acc8b4cf2241956857676838fd386e
syulyul <syul9800@gmail.com>
2023-06-30 01:47:55 +0900

    edit_report-28

--- report/app/src/main/java/bitcamp/report/App.java
+++ report/app/src/main/java/bitcamp/report/App.java
@@ -1,11 +1,10 @@
 package bitcamp.report;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import bitcamp.DataOutputStream;
+import bitcamp.io.DataInputStream;
+import bitcamp.io.DataOutputStream;
 import bitcamp.report.handler.BoardAddListener;
 import bitcamp.report.handler.BoardDeleteListener;
 import bitcamp.report.handler.BoardDetailListener;
@@ -124,30 +123,17 @@ public class App {
 
   private void loadMember() {
     try {
-      FileInputStream in = new FileInputStream("member.data");
+      DataInputStream in = new DataInputStream("member.data");
 
-      int size = in.read() << 8;
-      size |= in.read();
-
-      byte[] buf = new byte[1000];
+      int size = in.readShort();
 
       for (int i = 0; i < size; i++) {
         Member member = new Member();
-        member.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
-
-        int length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        member.setName(new String(buf, 0, length, "UTF-8"));
-
-        length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        member.setPhone(new String(buf, 0, length, "UTF-8"));
-
-        length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        member.setPassword(new String(buf, 0, length, "UTF-8"));
-
-        member.setPosition((char) (in.read() << 8 | in.read()));
+        member.setNo(in.readInt());
+        member.setName(in.readUTF());
+        member.setPhone(in.readUTF());
+        member.setPassword(in.readUTF());
+        member.setPosition(in.readChar());
 
         memberList.add(member);
       }
@@ -163,26 +149,16 @@ public class App {
 
   private void loadItem() {
     try {
-      FileInputStream in = new FileInputStream("item.data");
+      DataInputStream in = new DataInputStream("item.data");
 
-      int size = in.read() << 8;
-      size |= in.read();
-
-      byte[] buf = new byte[1000];
+      int size = in.readShort();
 
       for (int i = 0; i < size; i++) {
         Item item = new Item();
-        item.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
-
-        int length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        item.setName(new String(buf, 0, length, "UTF-8"));
-
-        item.setPrice(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
-
-        length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        item.setType(new String(buf, 0, length, "UTF-8"));
+        item.setNo(in.readInt());
+        item.setName(in.readUTF());
+        item.setPrice(in.readInt());
+        item.setType(in.readUTF());
 
         itemList.add(item);
       }
@@ -198,38 +174,19 @@ public class App {
 
   private void loadBoard(String filename, List<Board> list) {
     try {
-      FileInputStream in = new FileInputStream(filename);
+      DataInputStream in = new DataInputStream(filename);
 
-      int size = in.read() << 8;
-      size |= in.read();
-
-      byte[] buf = new byte[1000];
+      int size = in.readShort();
 
       for (int i = 0; i < size; i++) {
         Board board = new Board();
-        board.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
-
-        int length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        board.setTitle(new String(buf, 0, length, "UTF-8"));
-
-        length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        board.setContent(new String(buf, 0, length, "UTF-8"));
-
-        length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        board.setWriter(new String(buf, 0, length, "UTF-8"));
-
-        length = in.read() << 8 | in.read();
-        in.read(buf, 0, length);
-        board.setPassword(new String(buf, 0, length, "UTF-8"));
-
-        board.setViewCount(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
-
-        board.setCreatedDate((long) in.read() << 56 | (long) in.read() << 48
-            | (long) in.read() << 40 | (long) in.read() << 32 | (long) in.read() << 24
-            | (long) in.read() << 16 | (long) in.read() << 8 | in.read());
+        board.setNo(in.readInt());
+        board.setTitle(in.readUTF());
+        board.setContent(in.readUTF());
+        board.setWriter(in.readUTF());
+        board.setPassword(in.readUTF());
+        board.setViewCount(in.readInt());
+        board.setCreatedDate(in.readLong());
 
         list.add(board);
       }
@@ -247,34 +204,14 @@ public class App {
       DataOutputStream out = new DataOutputStream("member.data");
 
       // 저장할 데이터의 개수를 먼저 출력한다.
-      int size = memberList.size();
-      out.write(size >> 8);
-      out.write(size);
+      out.writeShort(memberList.size());
 
       for (Member member : memberList) {
         out.writeInt(member.getNo());
-
-        byte[] bytes = member.getName().getBytes("UTF-8");
-        // 출력할 바이트의 개수를 2바이트로 표시한다.
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-
-        // 문자열의 바이트를 출력한다.
-        out.write(bytes);
-
-        bytes = member.getPhone().getBytes("UTF-8");
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-        out.write(bytes);
-
-        bytes = member.getPassword().getBytes("UTF-8");
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-        out.write(bytes);
-
-        char position = member.getPosition();
-        out.write(position >> 8);
-        out.write(position);
+        out.writeUTF(member.getName());
+        out.writeUTF(member.getPhone());
+        out.writeUTF(member.getPassword());
+        out.writeChar(member.getPosition());
       }
       out.close();
     } catch (Exception e) {
@@ -284,38 +221,16 @@ public class App {
 
   private void saveItem() {
     try {
-      FileOutputStream out = new FileOutputStream("item.data");
+      DataOutputStream out = new DataOutputStream("item.data");
 
       // 저장할 데이터의 개수를 먼저 출력한다.
-      int size = itemList.size();
-      out.write(size >> 8);
-      out.write(size);
+      out.writeShort(itemList.size());
 
       for (Item item : itemList) {
-        int no = item.getNo();
-        out.write(no >> 24);
-        out.write(no >> 16);
-        out.write(no >> 8);
-        out.write(no);
-
-        byte[] bytes = item.getName().getBytes("UTF-8");
-        // 출력할 바이트의 개수를 2바이트로 표시한다.
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-
-        // 문자열의 바이트를 출력한다.
-        out.write(bytes);
-
-        int price = item.getPrice();
-        out.write(price >> 24);
-        out.write(price >> 16);
-        out.write(price >> 8);
-        out.write(price);
-
-        bytes = item.getType().getBytes("UTF-8");
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-        out.write(bytes);
+        out.writeInt(item.getNo());
+        out.writeUTF(item.getName());
+        out.writeInt(item.getPrice());
+        out.writeUTF(item.getType());
       }
       out.close();
     } catch (Exception e) {
@@ -325,57 +240,19 @@ public class App {
 
   private void saveBoard(String filename, List<Board> list) {
     try {
-      FileOutputStream out = new FileOutputStream(filename);
+      DataOutputStream out = new DataOutputStream(filename);
 
       // 저장할 데이터의 개수를 먼저 출력한다.
-      int size = list.size();
-      out.write(size >> 8);
-      out.write(size);
+      out.writeShort(list.size());
 
       for (Board board : list) {
-        int no = board.getNo();
-        out.write(no >> 24);
-        out.write(no >> 16);
-        out.write(no >> 8);
-        out.write(no);
-
-        byte[] bytes = board.getTitle().getBytes("UTF-8");
-        // 출력할 바이트의 개수를 2바이트로 표시한다.
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-        // 문자열의 바이트를 출력한다.
-        out.write(bytes);
-
-        bytes = board.getContent().getBytes("UTF-8");
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-        out.write(bytes);
-
-        bytes = board.getWriter().getBytes("UTF-8");
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-        out.write(bytes);
-
-        bytes = board.getPassword().getBytes("UTF-8");
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-        out.write(bytes);
-
-        int viewCount = board.getViewCount();
-        out.write(viewCount >> 24);
-        out.write(viewCount >> 16);
-        out.write(viewCount >> 8);
-        out.write(viewCount);
-
-        long createdDate = board.getCreatedDate();
-        out.write((int) (createdDate >> 56));
-        out.write((int) (createdDate >> 48));
-        out.write((int) (createdDate >> 40));
-        out.write((int) (createdDate >> 32));
-        out.write((int) (createdDate >> 24));
-        out.write((int) (createdDate >> 16));
-        out.write((int) (createdDate >> 8));
-        out.write((int) (createdDate));
+        out.writeInt(board.getNo());
+        out.writeUTF(board.getTitle());
+        out.writeUTF(board.getContent());
+        out.writeUTF(board.getWriter());
+        out.writeUTF(board.getPassword());
+        out.writeInt(board.getViewCount());
+        out.writeLong(board.getCreatedDate());
       }
       out.close();
 
