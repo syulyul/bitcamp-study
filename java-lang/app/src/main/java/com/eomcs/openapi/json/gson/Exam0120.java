e72170416402c722a6372806eab2fa9b53cdda54
syulyul <syul9800@gmail.com>
2023-07-04 09:28:48 +0900

    edit_report-35

--- java-lang/app/src/main/java/com/eomcs/openapi/json/gson/Exam0120.java
+++ java-lang/app/src/main/java/com/eomcs/openapi/json/gson/Exam0120.java
@@ -8,7 +8,7 @@ public class Exam0120 {
 
     // 1) JSON 문자열 준비
     String jsonStr =
-        "{\"no\":100,\"name\":\"홍길동\",\"email\":\"hong@test.com\",\"password\":\"1111\",\"photo\":\"hong.gif\",\"tel\":\"010-2222-1111\",\"registeredDate\":\"1월 24, 2022\"}";
+        "{\"no\":100,\"name\":\"홍길동\",\"email\":\"hong@test.com\",\"password\":\"1111\",\"photo\":\"hong.gif\",\"tel\":\"010-2222-1111\",\"registeredDate\":\"7월 3, 2023\"}";
 
     // 2) JSON 처리 객체 준비
     Gson gson = new Gson();
