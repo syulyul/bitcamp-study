2d23ea3a710efd6feefa8e75c18b732859c9b262
syulyul <syul9800@gmail.com>
2023-07-12 09:03:37 +0900

    update_report-41

--- report/app-39-server/src/main/java/bitcamp/report/ServerApp05.java
+++ report/app-39-server/src/main/java/bitcamp/report/ServerApp05.java
@@ -17,13 +17,14 @@ import bitcamp.report.dao.MemberListDao;
 // 2) 클라이언트가 요청한 DAO 객체와 메서드를 찾는다.
 // 3) 메서드의 파라미터와 리턴 타입을 알아내기
 // 4) 메서드 호출 및 리턴 값 받기
-public class ServerApp04 {
+// 5) 리팩토링
+public class ServerApp05 {
   int port;
   ServerSocket serverSocket;
 
   HashMap<String, Object> daoMap = new HashMap<>();
 
-  public ServerApp04(int port) throws Exception {
+  public ServerApp05(int port) throws Exception {
     this.port = port;
 
     daoMap.put("member", new MemberListDao("member.json"));
@@ -42,7 +43,7 @@ public class ServerApp04 {
       return;
     }
 
-    ServerApp04 app = new ServerApp04(Integer.parseInt(args[0]));
+    ServerApp05 app = new ServerApp05(Integer.parseInt(args[0]));
     app.execute();
     app.close();
 
@@ -81,44 +82,48 @@ public class ServerApp04 {
         continue;
       }
 
-      Method[] methods = dao.getClass().getDeclaredMethods();
-      Method method = null;
-      for (int i = 0; i < methods.length; i++) {
-        if (methods[i].getName().equals(methodName)) {
-          method = methods[i];
-          break;
-        }
-      }
-
+      // DAO 객체에서 메서드 찾기
+      Method method = findMethod(dao, methodName);
       if (method == null) {
         out.writeUTF(
             new ResponseEntity().status(ResponseEntity.ERROR).result("메서드를 찾을 수 없습니다.").toJson());
         continue;
       }
 
-      Parameter[] params = method.getParameters();
+      // DAO 메서드 호출하기
+      Object result = call(dao, method, request);
 
-      // 메서드를 호출
-      Object returnValue = null;
-
-      if (params.length > 0) {
-        // => 클라이언트가 보낸 JSON 데이터를 메서드의 파라미터 값으로 deserialize 한다.
-        Object arg = request.getObject(params[0].getType());
-        returnValue = method.invoke(dao, arg);
-      } else {
-        returnValue = method.invoke(dao);
-      }
-
-      // 메서드 호출 결과를 클라이언트에게 보낸다.
       ResponseEntity response = new ResponseEntity();
       response.status(ResponseEntity.SUCCESS);
-      response.result(returnValue);
+      response.result(result);
       out.writeUTF(response.toJson());
     }
 
     out.close();
     in.close();
     socket.close();
+  }
+
+  // 메서드 찾기
+  public Method findMethod(Object obj, String methodName) {
+    Method[] methods = obj.getClass().getDeclaredMethods();
+    for (int i = 0; i < methods.length; i++) {
+      if (methods[i].getName().equals(methodName)) {
+        return methods[i];
+      }
+    }
+    return null;
+  }
+
+  // 메서드 호출하기
+  public static Object call(Object obj, Method method, RequestEntity request) throws Exception {
+    Parameter[] params = method.getParameters();
+
+    if (params.length > 0) {
+      return method.invoke(obj, request.getObject(params[0].getType()));
+    } else {
+      return method.invoke(obj);
+    }
   }
 }
 
