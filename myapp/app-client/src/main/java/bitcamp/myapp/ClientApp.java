a443e0cf3fcf216da8d0576e41b339cbd3c9d14b
syulyul <syul9800@gmail.com>
2023-07-24 12:05:18 +0900

    230724_update_47-myapp

--- myapp/app-client/src/main/java/bitcamp/myapp/ClientApp.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/ClientApp.java
@@ -35,8 +35,8 @@ public class ClientApp {
 
   public ClientApp(String ip, int port) throws Exception {
 
-    Connection con = DriverManager.getConnection("jdbc:mysql://study:1111@localhost:3306/studydb" // JDBC
-                                                                                                  // URL
+    Connection con = DriverManager.getConnection(
+        "jdbc:mysql://study:1111@localhost:3306/studydb" // JDBC URL
     );
 
     this.memberDao = new MySQLMemberDao(con);
